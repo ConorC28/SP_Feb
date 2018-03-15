8086889eec1bbc17ecedfa7d4205edb0fe78223c
ConorC28 <conlonconor@gmail.com>
2018-03-15 20:02:27 +0000

    user_rating added

--- djangorest/api/models.py
+++ djangorest/api/models.py
@@ -4,6 +4,7 @@ from rest_framework.validators import UniqueValidator
 from django.contrib.auth.models import User
 from multiselectfield import MultiSelectField          # Allows user multichoice for model, console - pip install django-multiselectfield
 
+
 CONSOLE_CHOICES = (('Playststion', 'Playstation'), # The first element is what is added to the db and the second is what displays for the user selection
 				('Commodore 64', 'Commodore 64'),
 				('Nes', 'Nes'),
@@ -23,6 +24,28 @@ CONSOLE_CHOICES = (('Playststion', 'Playstation'), # The first element is what i
 				('Sony Playstation','Sony Playstation'),
 				('Sony Playstation 2','Sony Playstation 2'),
 				('Microsoft Xbox', 'Microsoft Xbox'))
+				
+RATING_CHOICES = (('0.5', '0.5'), # The first element is what is added to the db and the second is what displays for the user selection
+				('1.0', '1.0'),
+				('1.5', '1.5'),
+				('2.0', '2.0'),
+				('2.5', '2.5'),
+				('3.0', '3.0'),
+				('3.5', '3.5'),
+				('4.0', '4.0'),
+				('4.5', '4.5'),
+				('5.0', '5.0'),
+				('5.5', '5.5'),
+				('6.0', '6.0'),
+				('6.5', '6.5'),
+				('7.0', '7.0'),
+				('7.5', '7.5'),
+				('8.0', '8.0'),
+				('8.5', '8.5'),
+				('9.0', '9.0'),
+				('9.5', '9.5'),
+				('10', '10'))
+				
 
 #COLLECTOR_STATUS_CHOICES = (('item_key1', 'Keeping'),
 #			('item_key2', 'Trading'),
@@ -31,8 +54,9 @@ CONSOLE_CHOICES = (('Playststion', 'Playstation'), # The first element is what i
 class Gameslist(models.Model):
 	title = models.CharField(max_length=225, blank=False, unique=False)
 	console = MultiSelectField(choices=CONSOLE_CHOICES)
-	user_rating = models.CharField(max_length=225, blank=False, unique=False)
+	user_rating = MultiSelectField(choices=RATING_CHOICES)
 	#collector_status = MultiSelectField(choices=COLLECTOR_STATUS_CHOICES)
+	#release_date = models.DateField(default=datetime.now, blank=True)
 	release_date = models.CharField(max_length=10, blank=False, unique=False)
 	description = models.CharField(max_length=5000, blank=False, unique=False)
 	fond_memories = models.CharField(max_length=10000, blank=False, unique=False)
